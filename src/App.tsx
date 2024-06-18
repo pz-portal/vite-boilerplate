a286dd4d1f9f6a16a16e51c9abac3f852a1d8dd0
pz-hypesaints <zheng.pan@hypesaints.xyz>
2024-06-18 18:09:35 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       method: 'web_app_open_invoice',
       event: 'invoice_closed',
       params: {
-        slug: '-3c_9l1IiVfJAAAA2hMloF5C4UM'
+        slug: 'l5nODl1IiVfMAAAAjN2ApZgKRZM'
       },
     });
   }
